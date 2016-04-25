65c671a6dd49933607b1eae41a5c3fbd7c03f095
Elliott Mantock <emantock@gmail.com>
2016-04-25 15:25:23 -0400

    removed latlong stuff from weather

--- weather.sh
+++ weather.sh
@@ -8,10 +8,6 @@ metric='imperial' && unit='F'
 ipinfo=$(curl -s ipinfo.io)
 city=$(echo $ipinfo | jq -r '.city')
 cityid=$(grep -m 1 "$city" ~/.i3/city.list.us.json | jq -r '._id')
-latlong=$(echo $ipinfo | jq -r '.loc')
-# Parse the latitude and longitude into their own values
-lat=$(echo `expr "$latlong" : '^\(.*\),'`)
-long=$(echo `expr "$latlong" : '^.*[,]\(.*\)$'`)
 
 weather=$(curl -s http://api.openweathermap.org/data/2.5/weather\?id=${cityid}\&units\=${metric}\&APPID\=$(cat ~/.i3/appid.txt))
 temperature=$(printf '%s' $(echo $weather | jq '.main.temp'))
