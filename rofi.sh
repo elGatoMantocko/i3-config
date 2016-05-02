e4657e71c1f13e1e110e7d2067054c9d528e9d37
Elliott Mantock <emantock@gmail.com>
2016-05-01 23:13:02 -0400

    removed readme and rofi script

--- rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rofi -show run -width 2560 -fg "#B0E670" -bg "#14161F" -hlfg "#14161F" -hlbg "#B0E670" -bc "#14161F" -location 0 -lines 19 -bw 0 -font "MonacoB 16" -padding 900 -separator-style none -opacity 80
-
